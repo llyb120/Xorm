f8853787a49343f8a52492b2616ff938a3c0deec
company_bin <tiemaocsdn@qq.com>
2017-09-15 13:34:26 +0800

    fix

--- build/decorator/PrimaryGeneratedColumn.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PrimaryColumn_1 = require("./PrimaryColumn");
-function PrimaryGeneratedColumn(column) {
-    return PrimaryColumn_1.PrimaryColumn(column);
-}
-exports.PrimaryGeneratedColumn = PrimaryGeneratedColumn;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUHJpbWFyeUdlbmVyYXRlZENvbHVtbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9kZWNvcmF0b3IvUHJpbWFyeUdlbmVyYXRlZENvbHVtbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLG1EQUFnRDtBQUNoRCxnQ0FBdUMsTUFBWTtJQUMvQyxNQUFNLENBQUMsNkJBQWEsQ0FBQyxNQUFNLENBQUMsQ0FBQztBQUNqQyxDQUFDO0FBRkQsd0RBRUMifQ==
