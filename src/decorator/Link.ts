82c03e38421bf2bde70d5470b8416f03a7102db3
company_bin <tiemaocsdn@qq.com>
2017-09-15 11:20:30 +0800

    增加一对一的关联

--- src/decorator/Link.ts
+++ src/decorator/Link.ts
@@ -1,13 +1,5 @@
 import { Entity, EntityDescirption, EntityMap, InitEntityDescirption } from './XEntity';
 
-export class test<T>{
-    static cubi<T>(a: new () => T, b: A<T>): void {
-
-    }
-
-}
-
-
 interface LinkOption<T> {
     from?: (item: any) => any,
     to: (item: T) => any,
@@ -112,23 +104,58 @@ export function ManyToOne<T>(
 }
 
 export function OneToOne<T>(
-    entity: Entity<T>,
-    targetKey: string,
-    fromKey: string
-    // toKey: string
+    entity: { new(): T },
+    // link : (item : T) => any
+    option: LinkOption<T>
+    // fromKey : (item : T) => any,
+    // toKey : (item : T) => any,
+    // rLink : (item : T) => any
+    // targetKey: string, fromKey: string
 ): Function {
-    console.log(arguments);
     return function (proto: Object, key: string) {
+        console.log(option)
+        var fromKey = makeFactory(option.from) || key;
+        var toKey = makeFactory(option.to);
+        if (!toKey) {
+            throw new Error("没有找到to");
+        }
         var info: EntityDescirption = EntityMap.get(proto.constructor.name) || InitEntityDescirption();
-        var sp = targetKey.split(".").map(item => item.trim());
         info.external[key] = {
-            entity: sp[0],
+            entity: entity.name,
             fromKey: fromKey,
-            toKey: sp[1],
+            toKey: toKey,
             field: key,
             // key: linkKey,
-            type: "1v1"
+            type: "1v1",
         };
         EntityMap.set(proto.constructor.name, info);
+        //如果有反向关联，给反向表追加连接
+        var reverse = makeFactory(option.reverse);
+        if (reverse) {
+            var info: EntityDescirption = EntityMap.get(entity.name) || InitEntityDescirption();
+            info.external[reverse] = {
+                entity: proto.constructor.name,
+                fromKey: toKey,
+                toKey: fromKey,
+                field: reverse,
+                // key: linkKey,
+                type: "1v1",
+            };
+            EntityMap.set(entity.name, info);
+        }
     }
+    // console.log(arguments);
+    // return function (proto: Object, key: string) {
+    //     var info: EntityDescirption = EntityMap.get(proto.constructor.name) || InitEntityDescirption();
+    //     var sp = targetKey.split(".").map(item => item.trim());
+    //     info.external[key] = {
+    //         entity: sp[0],
+    //         fromKey: fromKey,
+    //         toKey: sp[1],
+    //         field: key,
+    //         // key: linkKey,
+    //         type: "nv1"
+    //     };
+    //     EntityMap.set(proto.constructor.name, info);
+    // }
 }
