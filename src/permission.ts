0240d84a99b4e55a3371010e82721f70f7ee4990
company_bin <tiemaocsdn@qq.com>
2017-09-20 17:50:35 +0800

    fix

--- src/permission.ts
+++ src/permission.ts
@@ -1,7 +1,17 @@
+import { X } from './x';
 /**
  * 授权
  */
 export class Permission{
 
+    constructor(
+        public role : string,
+        
+    ){
+
+    }
     
 }
+
+//注册一个超级用户
+X.createRole("su");
